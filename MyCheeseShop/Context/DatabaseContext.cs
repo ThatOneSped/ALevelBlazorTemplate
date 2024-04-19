086f7c8170be03efdc18fbe95a815d1abd31f2e2
ThatOneSped <jackdgn46@yahoo.com>
2024-04-19 10:28:09 +0100

    Got up to slide 9

--- MyCheeseShop/Context/DatabaseContext.cs
+++ MyCheeseShop/Context/DatabaseContext.cs
@@ -2,16 +2,29 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace MyCheeseShop.Context
 {
     public class DatabaseContext : IdentityDbContext<User>
     {
+        private IWebHostEnvironment _environment;
+
+        public DbSet<Cheese> Cheeses { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options, IWebHostEnvironment environment) : base(options)
+        {
+            _environment = environment;
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionbuilder)
         {
-            var folder = Environment.SpecialFolder.MyDocuments;
-            var path = Environment.GetFolderPath(folder);
-            var dbPath = Path.Join(path, "database.db");
-            optionbuilder.UseSqlite($"Data Source={dbPath}");
+            var folder = Path.Combine(_environment.WebRootPath, "database");
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+            optionbuilder.UseSqlite($"Data Source={folder}/cheese.db");
         }
     }
 }
