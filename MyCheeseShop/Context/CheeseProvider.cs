2d2958052e9ed437221bfe9aa88cf033a21c7fd4
ThatOneSped <jackdgn46@yahoo.com>
2024-05-01 10:30:55 +0100

    Created login page, began working on the cards that will display on my shop.

---
+++ MyCheeseShop/Context/CheeseProvider.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyCheeseShop.Model;
+using System.Threading.Tasks;
+
+namespace MyCheeseShop.Context
+{
+    public class CheeseProvider
+    {
+        private readonly DatabaseContext _context;
+        public CheeseProvider(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Cheese>> GetAllCheesesAsync()
+        {
+            return await _context.Cheeses.OrderBy(CheeseProvider => CheeseProvider.Name).ToListAsync();
+        }
+        public Cheese? GetCheese(int id)
+        {
+            return _context.Cheeses.Find(id);
+        }
+    }
+
+}
