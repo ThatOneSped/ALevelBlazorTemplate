ffa5a34ac13098ef084078367d0cbc26ca5c391a
ThatOneSped <jackdgn46@yahoo.com>
2024-05-03 10:33:06 +0100

    Slide 20, created the front page display, currently working on GetItems.

--- MyCheeseShop/Model/ShoppingCart.cs
+++ MyCheeseShop/Model/ShoppingCart.cs
@@ -1,7 +1,24 @@
-﻿namespace MyCheeseShop.Model
+﻿using System.Numerics;
+
+namespace MyCheeseShop.Model
 {
     public class ShoppingCart
     {
         public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public void AddItem(Cheese cheeses, int quantity)
+        {
+            //adding cheese to cart
+            var item = Items.FirstOrDefault(item => item.Cheese.Id == cheeses.Id);
+            if (item is null)
+                Items.Add(new OrderItem { Cheese = cheeses, Quantity = quantity });
+            else //if item already in cart, add 1
+                item.Quantity += quantity;
+        }
+
+        public int GetItems(Cheese cheeses)
+        {
+            var item = Items.FirstOrDefault(item => item.Cheese.Id == cheeses.Id);
+            return item?.Quantity ?? 0;
+        }
     }
 }
