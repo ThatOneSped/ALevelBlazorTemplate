24992acf2206de99fc72b0365890e3b63c2fe9c8
ThatOneSped <jackdgn46@yahoo.com>
2024-05-22 10:20:39 +0100

    Edited Register page, and fixed front page, images still arent displaying

--- MyCheeseShop/Model/Cheese.cs
+++ MyCheeseShop/Model/Cheese.cs
@@ -3,10 +3,10 @@
     public class Cheese
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Type { get; set; } 
-        public string Description { get; set; }
-        public string Strength { get; set; }
+        public string ?Name { get; set; }
+        public string ?Type { get; set; } 
+        public string ?Description { get; set; }
+        public string ?Strength { get; set; }
         public decimal Price { get; set; }
         public string? ImageUrl { get; set; }
     }
